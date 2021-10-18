68d0b639e29169ec9cd1a818e5bd534ee6a95572
MostafaIn <mostafa.hazareh@theshortcut.org>
2021-10-18 13:45:42 +0300

    back to root

--- src/services/events.js
+++ src/services/events.js
@@ -31,8 +31,8 @@ const getEvents = async () => {
     event.status === 'live' ? Object.assign(event, { status: 'upcoming' }) : event.status
   );
 
-  const eventsByCMS = await axios.get('https://admin.theshortcut.org/wp-json/wp/v2/events/?per_page=100');
-  let data = eventsByCMS.data.map((data) => data.acf);
+  const eventsByCMS = await axios.get('https://theshortcut.org/wp-json/wp/v2/events/');
+  let data = eventsByCMS?.data.map((data) => data.acf);
   let modifiedData = [];
   data.forEach((item) => {
     let dataObj = {
